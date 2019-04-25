1de80aa1cb2c61da9bc1d25d8bab74a26c8e7af9
Adam Gomulka <adam.gomulka4@gmail.com>
2019-04-25 14:31:45 -0600

    Remove commented out configs

--- configmgr.go
+++ configmgr.go
@@ -33,48 +33,6 @@ type Service struct {
 type Directive interface {
     handle() error
 }
-/*
-var Config = []Directive{
-    Deb{
-        Name: "nginx",
-        Install: true,
-        Upgrade: true,
-    },
-    Deb{
-        Name: "php5-fpm",
-        Install: true,
-        Upgrade: true,
-    },
-    File{
-       Path: "/etc/nginx/sites-available/default",
-       Owner: 1000,
-       Group: 1000,
-       Mode: 0664,
-       Directory: false,
-       Create: true,
-       Content: "server {\n\tlisten 80 default_server;\n\troot /usr/share/nginx/html;\n\tindex index.php;\n\tlocation ~ \\.php$ {\n\t\ttry_files $uri =404;\n\t\tfastcgi_split_path_info ^(.+\\.php)(/.+)$;\n\t\tinclude fastcgi_params;\n\t\tfastcgi_pass unix:/var/run/php5-fpm.sock;\n\t\tfastcgi_index index.php;\n\t\tfastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;\n\t}\n}",
-    },
-    File{
-        Path: "/usr/share/nginx/html/index.php",
-        Owner: 1000,
-        Group: 1000,
-        Mode: 0664,
-        Directory: false,
-        Create: true,
-        Content: "<?php\nheader('Content-Type: text/plain');\necho 'Hello, world!';",
-    },
-    Service{
-        Name: "php5-fpm",
-        Running: true,
-        Restart: true,
-    },
-    Service{
-        Name: "nginx",
-        Running: true,
-        Restart: true,
-    },
-}
-*/
 
 func (f File) handle() (e error) {
     _, e = os.Open(f.Path)
